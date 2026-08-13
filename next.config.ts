1bd8887fb4dfaab0c8f1a08ddf60131045375a7f
Egnoel <egnoel@hotmail.com>
2026-08-13 18:00:12 +0100

    profiles page

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images:{
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
       }
     ]
   }
